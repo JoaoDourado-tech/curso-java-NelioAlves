408d2c8559c32f5f36c92a86693668953f5bc66b
Joao Dourado <joaovitor7881@gmail.com>
2026-03-06 13:22:16 -0300

    Alterações nos exercícios usando-se de POO

--- src/secao08/Aula71e72.java
+++ src/secao08/Aula71e72.java
@@ -27,25 +27,17 @@ public class Aula71e72 {
         y.c = sc.nextDouble();
 
         System.out.println("CALCULANDO ÁREA TRIÂNGULO X e Y:");
-        double areaX = areaTriangulo(x.a, x.b, x.c);
-        double areaY = areaTriangulo(y.a, y.b, y.c);
+        double areaX = x.areaTriangulo();
+        double areaY = y.areaTriangulo();
 
         System.out.printf("Área do Triângulo X = %.2f%n", areaX);
         System.out.printf("Área do Triângulo Y = %.2f%n", areaY);
 
         System.out.println("Comparado Áreas obtidas...");
-        double maiorArea = compararArea(areaX, areaY);
+        double maiorArea = x.compararArea(y);
 
         System.out.printf("Maior Área: %.2f%n", maiorArea);
 
         sc.close();
-    }
-
-    public static double areaTriangulo(double l1, double l2, double l3) {
-        double p = ((l1 + l2 + l3) / 2);
-        return (Math.sqrt(p * (p - l1) * (p - l2) * (p - l3)));
-    }
-    public static double compararArea(double a1, double a2) {
-        return Math.max(a1, a2); // Math.max(X,Y) retorna o maior dos dois
     }
 }
--- src/secao08/Entities/Triangulo.java
+++ src/secao08/Entities/Triangulo.java
@@ -4,4 +4,14 @@ public class Triangulo {
     public double a;
     public double b;
     public double c;
-}
+
+    //Passando as Funções do Main para se tornarem Métodos no Class Triangulo
+    public double areaTriangulo() {
+        double p = ((a + b + c) / 2);
+        return (Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+    }
+
+    public double compararArea(Triangulo outro) {
+        return Math.max(this.areaTriangulo(), outro.areaTriangulo());
+    }
+    }
